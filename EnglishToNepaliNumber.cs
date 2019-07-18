d8b57a84d0090f222c70c47ac2be4cab38baf12b
solo-developer <48899799+solo-developer@users.noreply.github.com>
2019-07-19 00:13:14 +0545

    library added

---
+++ EnglishToNepaliNumber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishToNepaliNumber
+{
+    public class EnglishToNepaliNumber
+    {
+        private static string[] nepaliOnesNumbers = new string[] { "0", "१", "२", "३", "४", "५", "६", "७", "८", "९" };
+
+        public static string convertToNepaliNumber(decimal number)
+        {
+            string nepaliNumber = "";
+
+            string[] arrNumber = number.ToString().Split('.');
+
+            long wholePart = long.Parse(arrNumber[0]);
+
+            long[] numberChars = getIntArray(wholePart);
+
+            foreach (int englishNumber in numberChars)
+            {
+                nepaliNumber += getEquivalentNepaliNumber(englishNumber);
+            }
+
+            nepaliNumber = appendNumbersAfterDecimal(arrNumber, nepaliNumber);
+
+            return nepaliNumber;
+        }
+
+        private static string getEquivalentNepaliNumber(int numberChar)
+        {
+            return nepaliOnesNumbers[numberChar];
+        }
+
+        private static string appendNumbersAfterDecimal(string[] splitted_number_by_decimal, string nepaliNumber)
+        {
+            bool isNumberDecimal = splitted_number_by_decimal.Length == 2;
+            if (!isNumberDecimal)
+            {
+                return nepaliNumber;
+            }
+            nepaliNumber += ".";
+
+            int number_after_decimal = Convert.ToInt32(splitted_number_by_decimal[1]);
+
+            long[] numberChars = getIntArray(number_after_decimal);
+
+            foreach (int englishNumber in numberChars)
+            {
+                nepaliNumber += getEquivalentNepaliNumber(englishNumber);
+            }
+            return nepaliNumber;
+        }
+
+        private static long[] getIntArray(long num)
+        {
+            List<long> listOfInts = new List<long>();
+            while (num > 0)
+            {
+                listOfInts.Add(num % 10);
+                num = num / 10;
+            }
+            listOfInts.Reverse();
+            return listOfInts.ToArray();
+        }
+    }
+}
